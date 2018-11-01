a4ca87a0c30a68bea18f44e6268b20d2c347add4
Caio Flores <caiopflores@gmail.com>
2018-11-01 00:22:25 -0300

    Add demo
    Add a demo folder with build content.
    Add a rsync method in build script.

---
+++ docs/precache-manifest.6b1c7b78ead5aedae9ac52bebec871d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c15975326fac637d5dc1",
+    "url": "/static/js/main.c1597532.chunk.js"
+  },
+  {
+    "revision": "244c0c90919148007112",
+    "url": "/static/js/1.244c0c90.chunk.js"
+  },
+  {
+    "revision": "42185a33a090c2c96330857790c83c51",
+    "url": "/index.html"
+  }
+];
